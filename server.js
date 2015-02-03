2a055514e94d265895efa9da81a881ae8a5e70f2
san4osq <san4osq@ya.ru>
2015-02-04 01:05:50 +0200

    y

--- server.js
+++ server.js
@@ -11,17 +11,17 @@ var path = require('path');
 var app = express();
 
 
-app.engine('html', require('hogan-express'));
-app.set('view options', {layout: true});
-app.set('layout', 'layout');
-app.enable('view cache');
-app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'html');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
+//app.engine('html', require('hogan-express'));
+//app.set('view options', {layout: true});
+//app.set('layout', 'layout');
+//app.enable('view cache');
+//app.set('port', process.env.PORT || 3000);
+//app.set('view engine', 'html');
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+//app.use(express.bodyParser());
+//app.use(express.methodOverride());
+//app.use(app.router);
 app.use(express.static(path.join(__dirname, 'www')));
 
 //app.configure(function (){
