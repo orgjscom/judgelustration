5c34bf9e34d991225a800e1bd386185db54b6f5b
Alex Dykyi <alex.dykyi@gmail.com>
2015-02-16 20:11:17 +0200

    xml

--- server/sitemap/sitemap.js
+++ server/sitemap/sitemap.js
@@ -1,20 +1,20 @@
 exports.toSitemap = function(app){
 
-    var sm = require('sitemap');
-    var  sitemap = sm.createSitemap ({
-        hostname: 'http://judgelustration.com.ua',
-        cacheTime: 600000,
-        urls: [
-            {
-                url: '/index.html',  changefreq: 'daily', priority: 0.3
-            }
-        ]
-    });
-    app.get('/sitemap.xml', function(req, res) {
-        sitemap.toXML( function (xml) {
-            res.header('Content-Type', 'application/xml');
-            res.send( xml );
-        });
-    });
+    //var sm = require('sitemap');
+    //var  sitemap = sm.createSitemap ({
+    //    hostname: 'http://judgelustration.com.ua',
+    //    cacheTime: 600000,
+    //    urls: [
+    //        {
+    //            url: '/index.html',  changefreq: 'daily', priority: 0.3
+    //        }
+    //    ]
+    //});
+    //app.get('/sitemap.xml', function(req, res) {
+    //    sitemap.toXML( function (xml) {
+    //        res.header('Content-Type', 'application/xml');
+    //        res.send( xml );
+    //    });
+    //});
 
 };
