229e0dfd25efa846128c1b7b1b6827a111d97826
san4osq <san4osq@ya.ru>
2015-02-27 11:23:55 +0200

    fghfghfghfghfghfgh

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.engine('html', require('hogan-express'));
 app.set('view options', {layout: true});
 app.set('layout', 'layout');
 app.enable('view cache');
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 app.set('view engine', 'html');
 app.use(express.favicon());
 app.use(express.logger('dev'));
