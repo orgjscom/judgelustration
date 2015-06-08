3a1f80af0ff7e9a2a7bd3a8e443dd241799386b7
san4osq <san4osq@ya.ru>
2015-06-09 00:05:41 +0300

    test branch

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 var express  = require('express');
-//var mongoose = require('mongoose');
-//var mail = require('./email');
 var http = require('http');
 var path = require('path');
 var fs = require('fs');
 var json2xls = require('json2xls');
 var nodemailer = require('nodemailer');
-
 var app = express();
+
+
+var transporter = nodemailer.createTransport( {
+    host: "mx1.hostinger.com.ua",
+    secureConnection: true,
+    port: 2525,
+    auth: {
+        user: 'admin@blagoustriy.net',
+        pass: '11111111'
+    }
+});
+
+
 
 app.engine('html', require('hogan-express'));
 app.set('view options', {layout: true});
@@ -22,46 +32,85 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'www')));
 
+/**
+ *
+ * @type {string}
+ */
 var index  = "../www/index";
+
 app.get('/', function(req, res){
     res.render(index)
 });
 
+/**
+ *
+ * @type {string}
+ */
 var getQuestion  = "../www/question";
 app.get('/question', function(req, res){
     res.render(question, { "question": question })
 });
-
+/**
+ *
+ * @type {{}}
+ */
 var sendObj = {};
+
 app.post('/send', function(req, res){
+    /**
+     *
+     * @type {{message: *, number: (*|data.number|number|res.fields.number|qt.number|Ht.number), contact: (*|Document.contact)}}
+     */
     sendObj = {
         "message" :  req.body.message,
         "number"  :  req.body.number,
         "contact" :  req.body.contact
     };
+
     funcWriteJson(sendObj, res);
+
     try{
-     funcSendEmail(sendObj)
-    }catch(e){
+        funcSendEmail(sendObj)
+    } catch(e){
         console.log(e)
     }
+
     res.writeHead(302, {
         'Location': '/'
     });
     res.end();
 });
-
+/**
+ *
+ * @type {Array}
+ */
 var dataToWriteGlobal = [];
+/**
+ *
+ * @type {string}
+ */
 var url = 'www/ok/data.json';
 var readFile = fs.readFileSync( url );
 var jsonObj = JSON.parse( readFile );
+/**
+ *
+ * @type {Array}
+ */
 var arr = [];
 for( var i = 0; i < jsonObj.length; i++){
     arr.push(JSON.stringify(jsonObj[i]))
 }
-
+/**
+ *
+ * @param sendObj
+ * @param res
+ */
 var funcWriteJson = function(sendObj, res){
     arr.push(JSON.stringify(sendObj));
+    /**
+     *
+     * @type {string}
+     */
     var dataToWrite = "[" + arr + "]";
     fs.writeFileSync( url , dataToWrite );
     dataToWriteGlobal = JSON.stringify(jsonObj);
@@ -72,9 +121,21 @@ var funcWriteJson = function(sendObj, res){
 
 dataToWriteGlobal = JSON.stringify(jsonObj);
 
+/**
+ *
+ * @param dataToWrite
+ * @param res
+ */
 var funcWriteExcel = function(dataToWrite, res){
+
     var data = JSON.parse(dataToWrite);
+
+    /**
+     *
+     * @type {*|exports}
+     */
     var xls = json2xls(data);
+
     try{
         fs.writeFileSync('www/ok/data.xlsx', xls, 'binary');
         dataToWriteGlobal = dataToWrite;
@@ -87,28 +148,37 @@ var funcWriteExcel = function(dataToWrite, res){
     }
 };
 
-
+/**
+ *
+ * @param dataToWrite
+ * @returns {*}
+ */
 var dataToWriteNewFunc = function(dataToWrite){
     return dataToWrite;
 };
 
-
+/**
+ *
+ * @type {string}
+ */
 var all  = "../www/all";
+
 app.get('/24', function(req, res){
     res.render(all, { "data": dataToWriteGlobal })
 });
 
+/**
+ *
+ * @param sendObj
+ */
 var funcSendEmail = function(sendObj) {
 
-    var transporter = nodemailer.createTransport( {
-        host: "mx1.hostinger.com.ua",
-        secureConnection: true,
-        port: 2525,
-        auth: {
-            user: 'admin@blagoustriy.net',
-            pass: '11111111'
-        }
-    });
+
+
+    /**
+     *
+     * @type {string}
+     */
     var htmlMgs =
         "<hr>Повідомлення - "	        + sendObj.message +
         "<hr>№ справи або рішення - "	+ sendObj.number  +
@@ -117,6 +187,10 @@ var funcSendEmail = function(sendObj) {
         '<hr><b><a href="http://judgelustration.herokuapp.com/">перейти на головну judgelustration</a></b>' +
         '<hr><b><a href="http://judgelustration.herokuapp.com/ok/data.xlsx">скачати EXCEL judgelustration</a></b>';
 
+    /**
+     *
+     * @type {{from: string, to: string, subject: string, text: string, html: string}}
+     */
     var mailOptions = {
         from: 'judgelustration ✔ <judgelustration.com>',
         to: 'san4osq@ya.ru, yura.makedon@gmail.com, vgavdeev@gmail.com',
@@ -125,9 +199,10 @@ var funcSendEmail = function(sendObj) {
         html: htmlMgs
     };
     transporter.sendMail(mailOptions, function(error, info){
+
         if(error){
             console.log(error);
-        }else{
+        } else{
             console.log('Message sent: ' + info.response + mailOptions.html);
         }
     });
@@ -135,49 +210,8 @@ var funcSendEmail = function(sendObj) {
 };
 
 
-//var sitemap = require('./server/xml/xml');
-//sitemap.xml(app)
-
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
 });
-
-
-//app.configure(function (){
-//    app.use(express.cookieParser('keyboard cat'));
-//    app.use(express.session({ cookie: { maxAge: 60000 }}));
-//});
-
-//if ('development' == app.get('env')) {
-//    app.use(express.errorHandler());
-//    mongoose.connect('mongodb://localhost/clothing');
-//}
-
-//var MySignIn = new mongoose.Schema({
-//    name : String,
-//    pass : String
-//});
-
-//var user = mongoose.model('users',MySignIn);
-//
-//app.post('/register', function(req, res){
-//    new user({
-//        name : req.body.name,
-//        pass : req.body.pass
-//    }).save(function(err,doc){
-//            if(err) res.json(err);
-//            else res.statusCode = 302;
-//            res.setHeader("Location", "/users");
-//            res.end();
-//        });
-//});
-//
-//var admin  = "../www/admin/admin";
-//app.get('/admin', function(req, res){
-//    res.render(admin)
-//    mongoose.model('users').find(function(err, users){
-//        res.send(users);
-//    });
-//});
 
