329b6de71a4bd6aaf903df709b0d5c4a6ba941ea
san4osq <san4osq@ya.ru>
2015-06-08 11:46:00 +0300

    1111

--- www/js/zerif.js
+++ www/js/zerif.js
@@ -28,11 +28,11 @@
                 messageInput.addClass("notValidInput");
                 return false;
             }
-            //if( numberInput.val()   == "" ){
-            //    numberInput.removeClass("okValidInput");
-            //    numberInput.addClass("notValidInput");
-            //    return false;
-            //}
+            if( numberInput.val()   == "" ){
+                numberInput.removeClass("okValidInput");
+                numberInput.addClass("notValidInput");
+                return false;
+            }
         }
 
         objValidate = funcValidate();
